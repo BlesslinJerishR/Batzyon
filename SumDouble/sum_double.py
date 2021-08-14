361fdb7d43942f36998e32bbe1224c1ec6abaf12
Mastermindx33 <mastermindx33@gmail.com>
2021-08-14 14:44:48 +0530

    __init__sum_double__

---
+++ SumDouble/sum_double.py
@@ -0,0 +1,11 @@
+def sum_double(a, b):
+    if a == b:
+        return (a+b)*2
+    else:
+        return a+b
+
+# Test Cases
+
+print(sum_double(1, 2))
+print(sum_double(3, 2))
+print(sum_double(2, 2))
